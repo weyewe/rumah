b1503b6d00662526c9a431b4a618ef4b770b3e47
weyewe <w.yunnal@gmail.com>
2011-09-14 10:57:02 +0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rumah::Application.config.secret_token = '5958f45bdac5f53acb430fcab584d9392afafacc41c5789d573bb6313d5ebda75320da44c67c06693bd92b709d1d102d9a1c21a7d5500e2fc34dd327757013d3'
